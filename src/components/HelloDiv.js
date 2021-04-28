93186e889217b1f0f69731ef076c6c94aa951b25
Matthew Courtney <matthewdc182@gmail.com>
2021-04-27 22:42:37 -0400

    finsished the next activity

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -1,5 +1,15 @@
+import React from "react";
+
 function HelloDiv() {
-  return null;
+  return (
+    <div className="container">
+      <h1>Hello World my name is Matt Courtney</h1>
+    <ul className="li">Food</ul>
+    <ul className="li">Air</ul>
+    <ul className="li">Water</ul>
+    </div>
+  )
+       
 }
 
 export default HelloDiv;
