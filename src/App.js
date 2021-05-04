20d320234cececae3e00adc9eb37da7fda642719
Matthew Courtney <matthewdc182@gmail.com>
2021-05-03 23:26:26 -0400

    added in a state demo

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React from "react";
-import List from "./components/List";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-const groceries = [
-  {
-    id: 1,
-    name: "Milk",
-    purchased: true
-  },
-  {
-    id: 2,
-    name: "Eggs",
-    purchased: true
-  },
-  {
-    id: 3,
-    name: "Cheese",
-    purchased: false
-  },
-  {
-    id: 4,
-    name: "Cake Mix",
-    purchased: false
-  },
-  {
-    id: 5,
-    name: "Carrots",
-    purchased: false
-  },
-  {
-    id: 6,
-    name: "Juice",
-    purchased: true
-  }
-];
+import Counter from "./components/Counter";
 
 function App() {
-  return <List groceries={groceries} />;
+  return <Counter />;
 }
 
 export default App;
