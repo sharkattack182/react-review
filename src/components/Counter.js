33f81f46733b5ca4252380902946bef5ee4451b9
Matthew Courtney <matthewdc182@gmail.com>
2021-05-05 23:43:50 -0400

    added in the next assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,24 +1,14 @@
 import React from "react";
 
-// By extending the React.Component class, Counter inherits functionality from it
 class Counter extends React.Component {
-  // Setting the initial state of the Counter component
   state = {
     count: 0
   };
 
-  // handleIncrement increments this.state.count by 1
   handleIncrement = () => {
-    // We always use the setState method to update a component's state
     this.setState({ count: this.state.count + 1 });
   };
 
-  handleDecrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count - 1 });
-  };
-
-  // The render method returns the JSX that should be rendered
   render() {
     return (
       <div className="card text-center">
@@ -29,9 +19,6 @@ class Counter extends React.Component {
           <p className="card-text">Click Count: {this.state.count}</p>
           <button className="btn btn-primary" onClick={this.handleIncrement}>
             Increment
-          </button>
-          <button className="btn btn-danger" onClick={this.handleDecrement}>
-            Decrement
           </button>
         </div>
       </div>
