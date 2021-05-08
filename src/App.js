4ca86b71513662934bf0528ff4c02b44049663e6
Matthew Courtney <matthewdc182@gmail.com>
2021-05-07 20:15:49 -0400

    added in the next demo

--- src/App.js
+++ src/App.js
@@ -1,35 +1,8 @@
 import React from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import friends from "./friends.json";
+import Form from "./components/Form";
 
-class App extends React.Component {
-
-  state = {
-    friends: friends
-  };
-
-
-  deleteFriend = (id) => {
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-
-    this.setState({ friends })
-  }
-
-
-  render() {
-    return (
-      <Wrapper>
-        <Title>Friends List</Title>
-        {
-          this.state.friends.map(friend => {
-            return <FriendCard  name = {friend.name} occupation = {friend.occupation} location ={friend.location} image = {friend.image} id = {friend.id} deleteFriend = {this.deleteFriend}/>
-          })
-        }
-      </Wrapper>
-    );
-  }
+function App() {
+  return <Form />;
 }
 
 export default App;
